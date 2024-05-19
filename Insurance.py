499f5a9b0c3bc51897e8dc36404f6333d7229456
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2024-05-19 18:45:20 +0530

    Create Insurance.py

---
+++ Insurance.py
@@ -0,0 +1,13 @@
+from mrjob.job import mrjob
+import csv
+class MRInsurance(MRJob):
+    def mapper(self,_,line):
+        row=next(csv.reader([line]))
+        name=row[2]
+        yield name,1
+        
+    def reducer(self,key,values):
+        yield "Building name"+key,"frequency:"+str(sum(values))
+        
+if __name__=='__main__':
+    MRInsurance.run()
