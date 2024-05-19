841c36a0ab5fc9d59c4cd9d2b585ce2e6cc37d60
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2024-05-19 23:02:28 +0530

    Create male and female.py

---
+++ male and female.py
@@ -0,0 +1,21 @@
+from mrjob.job import MRJob
+
+class MREmployee(MRJob):
+    def mapper(self,_,line):
+       row=line.split("\t")
+       gender=row[3]
+       salary=float(row[8])
+       yield gender,salary
+       
+        
+    def reducer(self,key,value):
+       totalsal=0
+       cnt=0
+       for v in value:
+           totalsal+=v
+           cnt+=1
+       avgsal=totalsal/cnt
+       yield "Gender: "+key,"total employees: "+str(cnt)+"Average Salary: "+str(AVG)
+           
+if __name__=='__main__':
+    MREmployee.run()
