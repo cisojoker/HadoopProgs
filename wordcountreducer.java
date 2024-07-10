a3c37f8f323312810efcfd372985946f46bee7f5
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2024-07-10 12:13:24 +0530

    Create wordcountreducer.java

---
+++ wordcountreducer.java
@@ -0,0 +1,14 @@
+package wordcount;
+import java.util.*;
+import java.io.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+public class reducer extends MapReduceBase implements Reducer(Text,IntWritablre,Text,IntWritable){
+  public void reduce(Text Key,Iterator<IntWritable>value,outputCollector<Text,IntWritable>output,Reporter r){
+    int count=0;
+    while(value.hasNext()){
+      count+=value.next().get();
+    }
+    ouput.collect(new Text(key),new IntWritable(count));
+  }
+}
