7b4ccf5a3b6d59e18258012e3afdc2225b0732ac
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2024-07-10 11:52:54 +0530

    Create wordcountmapper.java

---
+++ wordcountmapper.java
@@ -0,0 +1,13 @@
+package wordcount;
+import java.io*;
+import java.util.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+public class mapper extends MapReduceBase implements Mapper<LongWritable,Text,Text,IntWritable>{
+public void map(LongWritable key,Text value,OutputCollector<Text,IntWritable>output,Reporter r)throws IOException{
+String line[]=value.toString().split(" ");
+  for(String a:line){
+    output.collect(new Text(a),new IntWritable(1));
+  }
+ }
+}
