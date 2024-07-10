b70f168e4f43a5b828c9d5acba96e348c0d7dd74
Madhur Verma <103921455+cisojoker@users.noreply.github.com>
2024-07-10 13:20:43 +0530

    Update employeemapper.java

--- employeemapper.java
+++ employeemapper.java
@@ -3,8 +3,8 @@ import java.io.*;
 import java.util.*;
 import org.apache.hadoop.io*;
 import org.apache.hadoop.mapred.*;
-public  mapper extends MapReduceBase implements Mapper<LongWritable,Text,Text,DoubleWritable>{
-  public void map(LongWritable key,Text value,OutputCollector<Text,DoubleWritable>,Reporter r)throws IOException{
+class  mapper extends MapReduceBase implements Mapper<LongWritable,Text,Text,DoubleWritable>{
+  public void map(LongWritable key,Text value,OutputCollector<Text,DoubleWritable>output,Reporter r)throws IOException{
     String[] line=value.toString().split("\\t");
     Double sal=Double.parseDouble(line[8]);
   output.collect(new Text(line[3]),new DoubleWritable(sal));
